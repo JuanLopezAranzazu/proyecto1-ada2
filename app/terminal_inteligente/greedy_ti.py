80c2007a9321d66040c2cc007eeaf182391e350c
JuanLopezAranzazu <juanlopezaranzazu@gmail.com>
2024-09-28 20:05:21 -0500

    Avances terminal inteligente

--- app/terminal_inteligente/greedy_ti.py
+++ app/terminal_inteligente/greedy_ti.py
@@ -1,3 +1,62 @@
 
 def greedy(stringA, stringB, operations_dict):
-  return 0, []
+  i,j = 0,0
+  cost=0
+  ops=[]
+
+  while(i<len(stringA) and j<len(stringB)):
+    cost_advance=float("inf")
+    cost_replace=float("inf")
+
+    if(stringA[i]==stringB[j]):
+      cost_advance=operations_dict["advance"]
+    else:
+      cost_replace=operations_dict["replace"]
+
+    cost_delete=operations_dict["delete"]
+    cost_insert=operations_dict["insert"]
+
+    # Seleccion de la operacion con menor costo
+    costs=[cost_advance, cost_delete, cost_replace, cost_insert]
+    min_cost=min(costs)
+    min_cost_index=costs.index(min_cost)
+
+    cost+=min_cost
+
+    if(min_cost_index==0):
+      ops.append("advance")
+      i+=1
+      j+=1
+    elif(min_cost_index==1):
+      ops.append("delete")
+      i+=1
+    elif(min_cost_index==2):
+      ops.append(f"replace {stringB[j]}")
+      i+=1
+      j+=1
+    elif(min_cost_index==3):
+      ops.append(f"insert {stringB[j]}")
+      j+=1
+  
+  # Caso en el que se recorren todas las letras de la cadena B
+  if(i<len(stringA)):
+    cost_delete=operations_dict["delete"]*(len(stringA)-i)
+    ops_delete=["delete"]*(len(stringA)-i)
+    cost_kill=operations_dict["kill"]
+    ops_kill=["kill"]
+
+    if(cost_delete<cost_kill):
+      cost+=cost_delete
+      ops+=ops_delete
+    else:
+      cost+=cost_kill
+      ops+=ops_kill
+  
+  # Caso en el que se recorren todas las letras de la cadena A
+  if(j<len(stringB)):
+    cost_insert=operations_dict["insert"]*(len(stringB)-j)
+    operations_insert=[f"insert {stringB[k]}" for k in range(j, len(stringB))]
+    cost+=cost_insert
+    ops+=operations_insert
+
+  return cost, ops
