372bf07560ec1d7080fc0cb6e0b6d49675883505
JuanLopezAranzazu <juanlopezaranzazu@gmail.com>
2024-10-22 22:45:11 -0500

    Agregando grafica de tiempo individual

--- app/graphics/auction_graphics.py
+++ app/graphics/auction_graphics.py
@@ -24,6 +24,16 @@ def plot_time_complexity():
       "B": 100,
       "offers": [[500, 100, 600], [450, 400, 800]],
     },
+    {
+      "A": 2000,
+      "B": 150,
+      "offers": [[500, 100, 600], [450, 400, 800], [600, 500, 1000]],
+    },
+    {
+      "A": 2000,
+      "B": 100,
+      "offers": [[450, 100, 300], [400, 280, 412], [500, 550, 618]],
+    }
   ]
 
   results = {
@@ -58,7 +68,7 @@ def plot_time_complexity():
   for algorithm, times in results.items():
     plt.plot(times, marker='o', linestyle='-', label=algorithm)
 
-  test_cases = ['Caso 1', 'Caso 2', 'Caso 3']
+  test_cases = [f'Caso {i + 1}' for i in range(len(test_cases))]
   plt.xticks(range(len(test_cases)), test_cases)
 
   plt.yscale('log')
@@ -69,5 +79,53 @@ def plot_time_complexity():
   plt.grid(True)
   plt.show()
 
+def plot_single_case():
+  test_case = {
+    "A": 1000,
+    "B": 100,
+    "offers": [[450, 100, 400], [400, 100, 400], [500, 400, 550]],
+  }
+
+  results = {
+    'brute_force': 0,
+    'dynamic_programming': 0,
+    'greedy': 0,
+  }
+
+  A = test_case["A"]
+  B = test_case["B"]
+  offers = test_case["offers"]
+
+  print(f'Caso de prueba: A={A}, B={B}, offers={offers}')
+
+  print('Fuerza bruta')
+  time = timeit.timeit(lambda: brute_force_auction.brute_force(A, B, offers), number=1)
+  results['brute_force'] = time
+  print(f'Tiempo de ejecución: {time}s')
+
+  print('Programación dinámica')
+  time = timeit.timeit(lambda: dynamic_programming_auction.dynamic_programming(A, B, offers), number=1)
+  results['dynamic_programming'] = time
+  print(f'Tiempo de ejecución: {time}s')
+
+  print('Algoritmo voraz')
+  time = timeit.timeit(lambda: greedy_auction.greedy(A, B, offers), number=1)
+  results['greedy'] = time
+  print(f'Tiempo de ejecución: {time}s')
+
+  plt.figure(figsize=(10, 5))
+  plt.bar(results.keys(), results.values(), color=['blue', 'orange', 'green'])
+  
+  for i, v in enumerate(results.values()):
+    plt.text(i, v, f'{v:.2f}', ha='center', va='bottom')
+
+  plt.xlabel('Algoritmos')
+  plt.ylabel('Tiempo de ejecución (s)')
+  plt.title('Comparación de algoritmos de la subasta pública')
+  plt.yscale('log')
+  plt.show()
+
+
 if __name__ == '__main__':
   plot_time_complexity()
+  plot_single_case()
--- app/graphics/terminal_graphics.py
+++ app/graphics/terminal_graphics.py
@@ -23,6 +23,16 @@ def plot_time_complexity():
       "stringA": "algorithm",
       "stringB": "altruistic",
       "operations_dict": {"advance": 1,"delete": 2,"replace": 3,"insert": 2,"kill": 1},
+    },
+    {
+      "stringA": "programming",
+      "stringB": "programmer",
+      "operations_dict": {"advance": 8,"delete": 2,"replace": 3,"insert": 2,"kill": 1},
+    },
+    {
+      "stringA": "computer",
+      "stringB": "computation",
+      "operations_dict": {"advance": 1,"delete": 21,"replace": 3,"insert": 6,"kill": 1},
     }
   ]
 
@@ -58,7 +68,7 @@ def plot_time_complexity():
   for algorithm, times in results.items():
     plt.plot(times, marker='o', linestyle='-', label=algorithm)
     
-  test_cases = ['Caso 1', 'Caso 2', 'Caso 3']
+  test_cases = [f'Caso {i + 1}' for i in range(len(test_cases))]
   plt.xticks(range(len(test_cases)), test_cases)
 
   plt.yscale('log')
@@ -69,5 +79,52 @@ def plot_time_complexity():
   plt.grid(True)
   plt.show()
 
+def plot_single_case():
+  test_case = {
+    "stringA": "programming",
+    "stringB": "programmer",
+    "operations_dict": {"advance": 8,"delete": 2,"replace": 3,"insert": 2,"kill": 1},
+  }
+
+  results = {
+    'brute_force': 0,
+    'dynamic_programming': 0,
+    'greedy': 0,
+  }
+
+  print(f'Caso de prueba: stringA={test_case["stringA"]}, stringB={test_case["stringB"]}, operations_dict={test_case["operations_dict"]}')
+
+  stringA = test_case["stringA"]
+  stringB = test_case["stringB"]
+  operations_dict = test_case["operations_dict"]
+
+  print('Fuerza bruta')
+  time = timeit.timeit(lambda: brute_force_terminal.brute_force(stringA, stringB, operations_dict), number=1)
+  results['brute_force'] = time
+  print(f'Tiempo de ejecución: {time}s')
+
+  print('Programación dinámica')
+  time = timeit.timeit(lambda: dynamic_programming_terminal.dynamic_programming(stringA, stringB, operations_dict), number=1)
+  results['dynamic_programming'] = time
+  print(f'Tiempo de ejecución: {time}s')
+
+  print('Algoritmo voraz')
+  time = timeit.timeit(lambda: greedy_terminal.greedy(stringA, stringB, operations_dict), number=1)
+  results['greedy'] = time
+  print(f'Tiempo de ejecución: {time}s')
+
+  plt.figure(figsize=(10, 5))
+  plt.bar(results.keys(), results.values(), color=['blue', 'orange', 'green'])
+  
+  for i, v in enumerate(results.values()):
+    plt.text(i, v, f'{v:.2f}', ha='center', va='bottom')
+
+  plt.xlabel('Algoritmos')
+  plt.ylabel('Tiempo de ejecución (s)')
+  plt.title('Comparación de algoritmos de la terminal inteligente')
+  plt.yscale('log')
+  plt.show()
+
 if __name__ == '__main__':
   plot_time_complexity()
+  plot_single_case()
